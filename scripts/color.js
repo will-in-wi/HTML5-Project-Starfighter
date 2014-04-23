9a2162fc06d2c67671e05779587ad968640807d1
William Johnston <wjohnston@mpr.org>
2014-04-23 09:10:45 -0500

    Color manipulation lib.

---
+++ scripts/color.js
@@ -0,0 +1,33 @@
+'use strict';
+
+function Color (red, green, blue) {
+
+    if (red > 255 || red < 0) {
+        throw 'Red color out of range: ' + red;
+    }
+
+    if (green > 255 || green < 0) {
+        throw 'Green color out of range: ' + green;
+    }
+
+    if (blue > 255 || blue < 0) {
+        throw 'Blue color out of range: ' + blue;
+    }
+
+    this.red = red;
+    this.green = green;
+    this.blue = blue;
+
+    this.hex = function () {
+        // Outputs hexadecimal RGB color.
+        return '#' + ("00" + this.red.toString(16)).substr(-2, 2) + ("00" + this.green.toString(16)).substr(-2, 2) + ("00" + this.blue.toString(16)).substr(-2, 2)
+    }
+
+    this.transform = function (amount) {
+        if (amount < -1 || amount > 1) {
+            throw 'Amount out of range.';
+        }
+
+        return new Color(Math.ceil(this.red * amount), Math.ceil(this.green * amount), Math.ceil(this.blue * amount));
+    }
+}
