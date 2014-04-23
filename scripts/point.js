8f5ad5c19ea7e54124c9b22059b9e3853076830a
William Johnston <wjohnston@mpr.org>
2014-04-23 10:03:55 -0500

    Allow moving of a point.

--- scripts/point.js
+++ scripts/point.js
@@ -6,7 +6,6 @@ function Point(x, y) {
     this.Y = y;
 
     this.move = function(x, y) {
-        this.X = this.X + x;
-        this.Y = this.Y + y;
+        return new Point(this.X + x, this.Y + y)
     }
 }
