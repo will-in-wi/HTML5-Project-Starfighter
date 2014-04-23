49d5b15a96bc3e3b8d0853848f9c24f3612757e8
William Johnston <wjohnston@mpr.org>
2014-04-23 10:23:38 -0500

    Add instruction text at the bottom.

--- scripts/scenes/briefing.js
+++ scripts/scenes/briefing.js
@@ -46,41 +46,7 @@ function Briefing(element) {
         var c2 = new Color(115, 117, 0);
         Widgets.header_box(loc.move(0, 320), c2, 'Additional Information', [element.additional_information]);
 
-        var textLoc;
-
-        state.ctx.textAlign = 'start';
-        state.ctx.font = "bolder 12pt 'Bitstream Vera Sans Mono'";
-        // if (person == 'none') {
-        //     state.ctx.fillStyle = 'black';
-        //     state.ctx.fillRect(loc.X, loc.Y, 600, 75);
-        //     textLoc = new Point(loc.X + 10, loc.Y + 10);
-
-        // } else {
-        //     state.ctx.fillStyle = 'blue';
-        //     state.ctx.fillRect(loc.X, loc.Y, 600, 75);
-        //     textLoc = new Point(loc.X + 60, loc.Y + 10);
-        //     state.ctx.drawImage(gameImages['face_' + person], loc.X + 5, loc.Y + 5);
-        // }
-
-        // Split up text into an array of lines
-
-        var lineLength = 600;
-
-        // var talk = split_text(text, lineLength);
-
-        // Draw lines
-        var i = 0;
-        // while (i < talk.length) {
-        //     // Draw shadow
-        //     state.ctx.fillStyle = 'black';
-        //     state.ctx.fillText(talk[i], textLoc.X, textLoc.Y + 11 + (i * 20));
-
-        //     // Draw text
-        //     state.ctx.fillStyle = 'white';
-        //     state.ctx.fillText(talk[i], textLoc.X, textLoc.Y + 10 + (i * 20));
-
-        //     i += 1;
-        // }
+        Widgets.text(loc.move(120, 490), 'Press [CTRL] or Fire to continue…');
     }
 
     this.main_loop = function () {
