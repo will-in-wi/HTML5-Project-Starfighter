10b5d69e6f72a718c72e7dbc11fa266cc96e94f0
William Johnston <wjohnston@mpr.org>
2014-04-24 08:24:10 -0500

    Add events lib

---
+++ scripts/events.js
@@ -0,0 +1,61 @@
+'use strict';
+
+/**
+ * @name Events
+ * @description creates a new Events object.  Currently used within {@link APMPlayer}, internal and external version + {@link Playlist}
+ * @constructor
+ */
+function Events() {
+    /**
+     * @name handlers
+     * @description array of event handler objects in form of { 'handler_name', function() {} }.
+     * @type Array.<Object>
+     * @fieldOf Events
+     */
+    this.handlers = [];
+
+    /**
+     * @name trigger
+     * @description fires all events handlers that match 'name' and passes eventArgs to each handler.
+     * @param {string} name name of event to fire
+     * @param {Object} eventArgs object literal to pass to all function handlers. Additional arguments will also be passed.
+     *
+     * @example APMPlayer.events.trigger(player.events.type.MEDIA_READY, { 'identifier' : this.ID });
+     *
+     * @methodOf Events
+     */
+    this.trigger = function (name, eventArgs) {
+        var i;
+        for (i = 0; i < this.handlers.length; i += 1) {
+            if (this.handlers[i].eventName === name) {
+                var args = Array.prototype.slice.call(arguments, 1);
+                this.handlers[i].eventHandler.apply(this, args);
+            }
+        }
+    },
+    /**
+     * @name addListener
+     * @description adds an event listener
+     * @param {string} name the name of the event to listen for
+     * @param {Object} handler function to fire when event is called.
+     *
+     * @example APMPlayer.events.addListener(APMPlayer.events.type.PLAYER_READY, function() {});
+     * @methodOf Events
+     */
+    this.addListener = function (name, handler) {
+        if (typeof (name) !== 'string' || typeof (handler) !== 'function') {
+            Debug.log("Invalid parameters when creating listener with the following arguments: 'Name': " + name + ", 'Handler': " + handler, Debug.type.error);
+        }
+        this.handlers.push({ "eventName" : name, "eventHandler" : handler });
+    },
+    /**
+     * @name removeListeners
+     * @description clears out all listeners in this events objec
+     *
+     * @example APMPlayer.events.removeListeners();
+     * @methodOf Events
+     */
+    this.removeListeners = function () {
+        this.handlers = [];
+    }
+}
